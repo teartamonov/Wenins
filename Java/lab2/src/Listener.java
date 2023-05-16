1b3e7cc532ab83691e3af67f81ffc49888d053c4
teartamonov <104139992+Wenins@users.noreply.github.com>
2023-05-16 22:05:39 +0300

    Update Listener.java

--- Java/lab2/src/Listener.java
+++ Java/lab2/src/Listener.java
@@ -1,16 +1,19 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
 
 class Listener implements ActionListener {
 
     JButton button1, button2;
     JPanel panel;
+    JLabel label;
     boolean visible = false;
-    Listener(JButton button1, JButton button2, JPanel panel){
+    Listener(JButton button1, JButton button2, JPanel panel, JLabel label){
         this.button1 = button1;
         this.button2 = button2;
         this.panel = panel;
+        this.label = label;
     }
 
     @Override
@@ -19,3 +22,4 @@ class Listener implements ActionListener {
         panel.setVisible(visible);
     }
 }
+
