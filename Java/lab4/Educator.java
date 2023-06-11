6d2fcfa742bcbb0f44e2b5b496a83ecf2c141c45
teartamonov <104139992+Wenins@users.noreply.github.com>
2023-06-11 22:21:51 +0300

    Update Educator.java

--- Java/lab4/Educator.java
+++ Java/lab4/Educator.java
@@ -1,13 +1,13 @@
 class Educator {
 
     public static void main(String []args){
-        AI start = new AI(new float[]{1,0,-1,1,0,-1,0,0.1f,0,0.5f,0,-0.4f,0,1,0,0,-0.001f,0});
+        AI start = new AI(new float[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
 
         int res = result(start);
         while(res<100000){
             AI best = start;
-            for(int i=0; i<3000; i++){
-                AI child = new AI(start, 50f);
+            for(int i=0; i<2000; i++){
+                AI child = new AI(start, 1f);
                 int child_res = result(child);
                 if(res<child_res){
                     res=child_res;
