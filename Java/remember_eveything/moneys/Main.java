ef5f731a8cbcbcc0ccdbd6cd24be9f9f46de4693
teartamonov <104139992+teartamonov@users.noreply.github.com>
2024-07-13 15:13:38 +0300

    Add files via upload

--- Java/remember_eveything/moneys/Main.java
+++ Java/remember_eveything/moneys/Main.java
@@ -2,6 +2,13 @@ package moneys;
 
 public class Main {
     public static void main(String[] args) {
+    Money m1 = new Money(12, 123);
+    Money m2 = new Money(9, 88);
+    m1.add(m2).get_money();
+    m1.subtract(m2).get_money();
+    m1.multiply(m2).get_money();
+    m1.divide(m2).get_money();
+
 
     }
 }
--- Java/remember_eveything/moneys/Money.java
+++ Java/remember_eveything/moneys/Money.java
@@ -1,14 +1,16 @@
 package moneys;
 
 public class Money {
-    long left;
-    int right;
+    private long left;
+    private int right;
 
     public Money(long left, int right){
         this.right = right;
         this.left = left;
     }
-
+    public void get_money(){
+        System.out.println(this.left + "." + this.right);
+    }
     public Money add(Money money){
         long l = this.left + money.left;
         int r;
@@ -16,7 +18,11 @@ public class Money {
         int len2 = String.valueOf(money.right).length();
         if (len1 > len2) r = (int)(money.right * Math.pow(10, len1 - len2) + this.right);
         else r = (int)(this.right * Math.pow(10, len2 - len1) + money.right);
-        if (String.valueOf(r).length() > Math.max(len1, len2)) l+=1;
+        int lenr = String.valueOf(r).length();
+        if (lenr > Math.max(len1, len2)) {
+            l+=1;
+            r = (int)(r % Math.pow(10, lenr - 1));
+        }
         return new Money(l,r);
     }
     public Money subtract(Money money){
@@ -26,7 +32,11 @@ public class Money {
         int len2 = String.valueOf(money.right).length();
         if (len1 > len2) r = (int)(this.right - money.right * Math.pow(10, len1 - len2));
         else r = (int)(this.right * Math.pow(10, len2 - len1) - money.right);
-        if (r < 0) l-=1;
+        int lenr = String.valueOf(r).length();
+        if (r < 0) {
+            l-=1;
+            r = (int)(Math.pow(10, lenr-1)) + r;
+        }
         return new Money(l,r);
     }
     public Money multiply(Money money){
@@ -39,12 +49,12 @@ public class Money {
         if (len1 > len2) {
             r1 = this.right;
             r2 = (int) (money.right * Math.pow(10, len1 - len2));
-            tmp = (this.left * len1 + r1) * (money.left * len1 + r2);
+            tmp = (this.left * (int)(Math.pow(10, len1)) + r1) * (money.left * (int)(Math.pow(10, len1)) + r2);
         }
         else {
             r1 = (int)(this.right * Math.pow(10, len2 - len1));
             r2 = money.right;
-            tmp = (this.left * len2 + r1) * (money.left * len2 + r2);
+            tmp = (this.left * (int)(Math.pow(10, len2)) + r1) * (money.left * (int)(Math.pow(10, len2)) + r2);
         }
         l = (long)(tmp/Math.pow(10, 2*len1));
         r = (int)(tmp % (Math.pow(10, 2*len1)));
@@ -55,22 +65,25 @@ public class Money {
         long l;
         int r1, r2;
         int r;
-        long tmp = 0;
+        double tmp;
         int len1 = String.valueOf(this.right).length();
         int len2 = String.valueOf(money.right).length();
         if (len1 > len2) {
             r1 = this.right;
             r2 = (int) (money.right * Math.pow(10, len1 - len2));
-            tmp = (this.left * len1 + r1) / (money.left * len1 + r2);
+            double dividend = (this.left * Math.pow(10, len1) + r1);
+            double divider = (money.left * Math.pow(10, len1) + r2);
+            tmp = dividend / divider;
         }
         else {
             r1 = (int)(this.right * Math.pow(10, len2 - len1));
             r2 = money.right;
-            tmp = (this.left * len2 + r1) / (money.left * len2 + r2);
+            double dividend = (this.left * Math.pow(10, len2) + r1);
+            double divider = (money.left * Math.pow(10, len2) + r2);
+            tmp = dividend / divider;
         }
-        double t = Math.pow(10, 2*Math.max(len1,len2));
-        l = (long)(tmp/t);
-        r = (int)(tmp % (t));
+        l = (long)(tmp);
+        r = (int)((tmp - l)*Math.pow(10,6));
 
         return new Money(l,r);
     }
