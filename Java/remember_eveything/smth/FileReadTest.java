5dd92f8106c77fb0281c6da87c8dbff11f9d9cb8
teartamonov <104139992+teartamonov@users.noreply.github.com>
2024-07-14 12:55:42 +0300

    Add files via upload

--- Java/remember_eveything/smth/FileReadTest.java
+++ Java/remember_eveything/smth/FileReadTest.java
@@ -1,18 +1,17 @@
 package smth;
 
-import java.io.FileWriter;
 import java.io.*;
 import java.util.Scanner;
 
 public class FileReadTest {
     public static void main(String args[])  {
         System.out.println(new File("src/smth/text.txt").exists());
+        System.out.println(new File("src/smth/buff.txt").exists());
         try {
             Scanner scanner = new Scanner(new FileReader("src/smth/text.txt"));
             while (scanner.hasNextLine()) {
                 System.out.println(scanner.nextLine());
                 }
-            scanner.close();
         }
         catch (IOException e){
             System.out.println("Ошибка при чтении" + e.getMessage());
@@ -21,10 +20,29 @@ public class FileReadTest {
             FileWriter writer = new FileWriter("src/smth/text.txt", true);
             writer.append("\nThis is Java documentation of some class");
             writer.flush();
-            writer.close();
         }
         catch (IOException e){
             System.out.println("Ошибка при записи " + e.getMessage());
+        }
+
+        try{
+            BufferedWriter buffWriter = new BufferedWriter(new FileWriter("src/smth/buff.txt", true));
+            buffWriter.append("\nThis is String I wrote with BufferedWriter!");
+            buffWriter.flush();
+        }
+        catch (IOException e){
+            System.out.println("Ошибка при записи " + e.getMessage());
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/smth/buff.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e){
+            System.out.println("Ошибка при чтении" + e.getMessage());
         }
 
 
